6f23d07b9c91df0f5a6f5eb8599d25c07244dde5
adrianvalerio <adrian_2004_valerio@hotmail.com>
2025-06-20 17:32:14 -0500

    chore: remove unused files for profiles

--- Frock-backend/Profiles/Domain/Model/Queries/GetAllProfilesQuery.cs
+++
@@ -1,6 +0,0 @@
-namespace Frock_backend.Profiles.Domain.Model.Queries;
-
-/// <summary>
-/// Get all profiles query 
-/// </summary>
-public record GetAllProfilesQuery();
--- Frock-backend/Profiles/Domain/Model/Queries/GetProfileByIdQuery.cs
+++
@@ -1,3 +0,0 @@
-namespace Frock_backend.Profiles.Domain.Model.Queries;
-
-public record GetProfileByIdQuery(int ProfileId);
