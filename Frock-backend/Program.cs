f2382e0ff88d3543a6dc5b10d17fa9e17f8165fd
AmirbarrabajaLeon <amircasan@gmail.com>
2025-06-20 20:21:38 -0500

    chore: changed cors allowed domain

--- Frock-backend/Program.cs
+++ Frock-backend/Program.cs
@@ -145,7 +145,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5174")//ajustar
+        policy.WithOrigins("http://localhost:5173")//ajustar
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
