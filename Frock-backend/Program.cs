90ecf1c1abac2ca35af053ee7cd0a484b6ec9573
AmirbarrabajaLeon <amircasan@gmail.com>
2025-07-08 00:47:23 -0500

    fix: update CORS policy, remove slash typo
    This commit refines the CORS policy by removing a trailing slash from the
    Netlify app URL, ensuring consistent allowed origins
    for cross-origin requests.

--- Frock-backend/Program.cs
+++ Frock-backend/Program.cs
@@ -216,7 +216,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "https://deft-tapioca-c27a9c.netlify.app/")//ajustar
+        policy.WithOrigins("http://localhost:5173", "https://deft-tapioca-c27a9c.netlify.app")//ajustar
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
